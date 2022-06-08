f7d3d72bb8b20f825bbbb3ce17c80231c1b8a833
CRClothier <c.r.clothier@gmail.com>
2022-06-07 22:14:03 -0500

    Implement routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'users_lists#users_list'
+  root 'users#index'
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
+  get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:id', to: 'posts#show'
 end
