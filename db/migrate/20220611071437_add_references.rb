adda59b6ec0a297ac262e767fea70d52126c8d4e
CRClothier <c.r.clothier@gmail.com>
2022-06-11 02:22:35 -0500

    Run db:migrate

--- db/migrate/20220611071437_add_references.rb
+++ db/migrate/20220611071437_add_references.rb
@@ -1,6 +1,6 @@
 class AddReferences < ActiveRecord::Migration[7.0]
   def change
-    dd_reference :posts, :users, null: false, foreign_key: true
+    add_reference :posts, :users, null: false, foreign_key: true
     add_reference :comments, :users, null: false, foreign_key: true
     add_reference :comments, :posts, null: false, foreign_key: true
     add_reference :likes, :users, null: false, foreign_key: true
