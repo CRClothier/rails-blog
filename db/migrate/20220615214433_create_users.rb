3ddb9f21ca2d33bef59e1cb885ae0eb7096af8a4
CRClothier <c.r.clothier@gmail.com>
2022-06-29 23:41:37 -0500

    Style views

--- db/migrate/20220615214433_create_users.rb
+++ db/migrate/20220615214433_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo
-      t.text :bio
-      t.integer :posts_counter
+      t.string :names
+      t.string :photos
+      t.text :bios
+      t.integer :posts_counters
 
       t.timestamps
     end
